26b14f7eab10e2e79916efb1ffbf344ff1f79d1c
ziferval <ilovemepaulina@gmail.com>
2025-09-25 16:35:45 +0100

    Create calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    int op, valor1, valor2, resultado;
+    printf("O que pretende fazer?\n ");
+    printf("1 - Soma\n ");
+    printf("2 - Subtração\n ");
+    printf("3 - Multiplicação\n ");
+    printf("4 - Divisão\n ");
+    scanf("%d", &op);
+    printf("Insira o primeiro valor: ");
+    scanf("%d", &valor1);
+    printf("Insira o segundo valor: ");
+    scanf("%d", &valor2);
+
+    switch (op){
+        case 1:
+            resultado=valor1+valor2;
+            printf("Resultado: %d.", resultado);
+            break;
+        case 2:
+            resultado=valor1-valor2;
+            printf("Resultado: %d.", resultado);
+            break;
+        case 3:
+            resultado=valor1*valor2;
+            printf("Resultado: %d.", resultado);
+            break;
+        case 4:
+            resultado=valor1/valor2;
+            printf("Resultado: %d.", resultado);
+            break;
+    }
+}
