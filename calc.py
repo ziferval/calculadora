96a5d4a368887aa87d86e45a71ef273d26c52134
beepboop92 <papeltemrazao@gmail.com>
2025-10-13 14:24:08 +0100

    Calculadora mas super eficiente
    + Python

---
+++ calc.py
@@ -0,0 +1,25 @@
+a = int(input("Numero 1: "))
+b = int(input("Numero 2: "))
+
+if a == 1 and b == 1:
+    print(1 + 1)
+elif a == 2 and b == 2:
+    print(2 + 2)
+elif a == 3 and b == 3:
+    print(3 + 3)
+elif a == 4 and b == 4:
+    print(4 + 4)
+elif a == 5 and b == 5:
+    print(5 + 5)
+elif a == 6 and b == 6:
+    print(6 + 6)
+elif a == 7 and b == 7:
+    print(7 + 7)
+elif a == 8 and b == 8:
+    print(8 + 8)
+elif a == 9 and b == 9:
+    print(9 + 9)
+elif a == 10 and b == 10:
+    print(10 + 10)
+else:
+    print("No matching condition found.")
